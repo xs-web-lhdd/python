925a3b7b1b549ac2bc275179f381b65bb8378895
凉风有信 <3042146237@qq.com>
2023-07-20 21:21:15 +0800

    feat: 卷积原理与操作、人脸关键点定位、疲劳检测

---
+++ opencv/卷积原理与操作/convolutions.py
@@ -0,0 +1,107 @@
+# 导入工具包
+from skimage.exposure import rescale_intensity
+import numpy as np
+import argparse
+import cv2
+
+"""
+filter2D(src, ddepth, kernel, dst=None, anchor=None, delta=None, borderType=None)
+Stc: 输入的原图像；
+ddepth: 表示目标图像的深度，默认是原来的图像深度相同，即-1；
+Kernel：表示卷积核，一个单通道的浮点型矩阵；如果想在不同的通道使用不同的kernel，可以先使用split（）函数将图像通道分开；
+Dst: 表示输出的目标图像；
+Anchor：内核基准点，默认值为（-1,-1），说明位于kernel的中心位置；类似于偏置。
+Delta：表示在将过滤后的像素存储到dst之前，向其添加可选值；
+BorderType：表示像素向外逼近的方法，默认值为BORDER_DEFAULT。
+"""
+
+
+def convolve(image, kernel):
+    # 输入图像和核的尺寸
+    (iH, iW) = image.shape[:2]
+    (kH, kW) = kernel.shape[:2]
+
+    # 选择pad，卷积后图像大小不变
+    pad = (kW - 1) // 2
+    # 重复最后一个元素，top, bottom, left, right,  cv2.BORDER_REPLICATE 使用边界进行扩充
+    image = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REPLICATE)
+    output = np.zeros((iH, iW), dtype="float32")
+
+    # 卷积操作
+    for y in np.arange(pad, iH + pad):
+        for x in np.arange(pad, iW + pad):
+            # 提取每一个卷积区域
+            roi = image[y - pad:y + pad + 1, x - pad:x + pad + 1]
+
+            # 内积运算
+            k = (roi * kernel).sum()
+
+            # 保存相应的结果
+            output[y - pad, x - pad] = k
+
+    # 将得到的结果放缩到[0, 255]，防止值超过 255
+    output = rescale_intensity(output, in_range=(0, 255))
+    output = (output * 255).astype("uint8")
+
+    return output
+
+
+# 指定输入图像
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+                help="path to the input image")
+args = vars(ap.parse_args())
+
+# 分别构建两个卷积核
+smallBlur = np.ones((7, 7), dtype="float") * (1.0 / (7 * 7))
+largeBlur = np.ones((21, 21), dtype="float") * (1.0 / (21 * 21))
+
+# 尝试不同的卷积核
+sharpen = np.array((
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]), dtype="int")
+
+laplacian = np.array((
+    [0, 1, 0],
+    [1, -4, 1],
+    [0, 1, 0]), dtype="int")
+
+sobelX = np.array((
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1]), dtype="int")
+
+sobelY = np.array((
+    [-1, -2, -1],
+    [0, 0, 0],
+    [1, 2, 1]), dtype="int")
+
+# 尝试不同结果
+kernelBank = (
+    ("small_blur", smallBlur),
+    ("large_blur", largeBlur),
+    ("sharpen", sharpen),
+    ("laplacian", laplacian),
+    ("sobel_x", sobelX),
+    ("sobel_y", sobelY)
+)
+
+# 简单起见，用灰度图来玩
+image = cv2.imread(args["image"])
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# 遍历每一个核
+for (kernelName, kernel) in kernelBank:
+    print("[INFO] applying {} kernel".format(kernelName))
+    # 自己计算的卷积:
+    convoleOutput = convolve(gray, kernel)
+    # -1 表示深度一致, opencv 计算的卷积
+    opencvOutput = cv2.filter2D(gray, -1, kernel)
+
+    # 分别展示结果
+    cv2.imshow("original", gray)
+    cv2.imshow("{} - convole".format(kernelName), convoleOutput)
+    cv2.imshow("{} - opencv".format(kernelName), opencvOutput)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
